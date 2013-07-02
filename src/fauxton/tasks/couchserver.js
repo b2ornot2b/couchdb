8e8b8b216ca8cd082ee0ae6fa27c68c0a63c380c
Garren Smith <garren.smith@gmail.com>
2013-07-02 12:05:44 +0200

    remove express

--- src/fauxton/tasks/couchserver.js
+++ src/fauxton/tasks/couchserver.js
@@ -13,18 +13,18 @@
 module.exports = function (grunt) {
   var log = grunt.log;
 
- grunt.registerTask("couchserver", 'Run a couch dev proxy server', function () {
+  grunt.registerTask("couchserver", 'Run a couch dev proxy server', function () {
     var fs = require("fs"),
-    path = require("path"),
-    httpProxy = require('http-proxy'),
-    express = require("express"),
-    options = grunt.config('couchserver'),
-    app = express();
+        path = require("path"),
+        http = require("http"),
+        httpProxy = require('http-proxy'),
+        send = require('send'),
+        options = grunt.config('couchserver');
 
     // Options
-    var dist_dir = options.dist || './dist/debug/';
-    var app_dir = './app';
-    var port = options.port || 8000;
+    var dist_dir = options.dist || './dist/debug/',
+        app_dir = './app',
+        port = options.port || 8000;
 
     // Proxy options with default localhost
     var proxy_settings = options.proxy || {
@@ -38,40 +38,35 @@ module.exports = function (grunt) {
     // inform grunt that this task is async
     var done = this.async();
 
-    // serve any javascript or css files from here assets dir
-    app.get(/assets/, function (req, res) {
-      res.sendfile(path.join('./',req.url));
-    });
-    
-    // serve any javascript or css files from dist debug dir
-    app.get(/\.css$|\/js\/|img/, function (req, res) {
-      res.sendfile(path.join(dist_dir,req.url));
-    });
-
-    app.get(/\.js$/, function (req, res) {
-      console.log('js', req.url);
-      res.sendfile(path.join(app_dir,req.url));
-    });
-
     // create proxy to couch for all couch requests
     var proxy = new httpProxy.HttpProxy(proxy_settings);
 
-    // serve main index file from here
-    // Also proxy out to the base CouchDB host for handle_welcome_req.
-    // We still need to reach the top level CouchDB host even through
-    // the proxy.
-    app.get('/', function (req, res) {
-      var accept = req.headers.accept.split(',');
-      if (accept[0] == 'application/json') {
-        proxy.proxyRequest(req, res);
-      } else {
-        res.sendfile(path.join(dist_dir, 'index.html'));
-      }
-    });
+    http.createServer(function (req, res) {
+      var url = req.url,
+          accept = req.headers.accept.split(','),
+          filePath;
 
-    app.all('*', function (req, res) {
+      if (!!url.match(/assets/)) {
+        // serve any javascript or css files from here assets dir
+        filePath = path.join('./',req.url);
+      } else if (!!url.match(/\.css$|\/js\/|img/)) {
+        // serve any javascript or css files from dist debug dir
+        filePath = path.join(dist_dir,req.url);
+      } else if (!!url.match(/\.js$|\.html$/)) {
+        // server js from app directory
+        console.log('js', req.url);
+        filePath = path.join(app_dir,req.url.replace('/_utils/fauxton/app',''));
+      } else if (url === '/' && accept[0] !== 'application/json') {
+        // serve main index file from here
+        filePath = path.join(dist_dir, 'index.html');
+      };
+
+      if (filePath) {
+        return send(req, filePath).pipe(res);
+      } 
+
       proxy.proxyRequest(req, res);
-    });
+    }).listen(port);
 
     // Fail this task if any errors have been logged
     if (grunt.errors) {
@@ -84,8 +79,6 @@ module.exports = function (grunt) {
     watch.stderr.pipe(process.stderr);
 
     log.writeln('Listening on ' + port);
-    app.listen(port);
-
   });
 
 };
